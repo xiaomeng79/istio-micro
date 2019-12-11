2e04c0c396de5a3589fd35fd174230973312702c
xiaomeng79 <496096100@qq.com>
2019-12-11 18:10:47 +0800

    修改采样率为1

--- cinit/common.go
+++ cinit/common.go
@@ -39,7 +39,7 @@ var Config = struct {
 	Trace struct { //  链路跟踪
 		Address       string  `default:"http://jaeger:14268/api/traces?format=jaeger.thrift"` //  http://jaeger:14268/api/traces?format=jaeger.thrift
 		ZipkinURL     string  `default:""`                                                    //  http://zipkin:9411/api/v1/spans
-		SamplingRate  float64 `default:"0.1"`                                                 //  采样率 0.01-1范围
+		SamplingRate  float64 `default:"1"`                                                   //  采样率 0.01-1范围
 		LogTraceSpans bool    `default:"false"`                                               //  日志
 	}
 	//  log config
